dac18a8c972caab2cf0295e966246336dbf4aeb5
anmedina-arg <andres.medina.arg@gmail.com>
2025-11-19 21:56:58 -0300

    update hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,18 @@ import ProductListContainer from "@/components/ProductListContainer";
 import Footer from "@/components/Footer";
 import FilterButtons from "@/components/FilterButtons";
 import HelpButton from "@/components/HelpButton";
+import Image from "next/image";
 
 export default function Home() {
 
   return (
     <div className="font-barlow flex flex-col min-h-screen px-2">
 
-      <div className="text-center mb-4">
-        <h1 className="text-2xl font-bold mb-2">Lista de precios y productos</h1>
+      <div className="text-center mb-4 mt-2 justify-center items-center flex flex-col gap-2">
+        <div className="flex items-center gap-2 ">
+          <Image src="https://res.cloudinary.com/dfwo3qi5q/image/upload/v1763599423/logo-og_pydhrd.png" alt="Market del cevil Logo" width={64} height={6} className="rounded-full" />
+          <h1 className="text-2xl font-bold">Market del Cevil</h1>
+        </div>
         <p className="text-sm max-w-md mx-auto text-balance">
           Selecciona los productos que quieres pedir y luego envía tu pedido por WhatsApp <HelpButton />
         </p>
