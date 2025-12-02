4a9c4d27e805ac92936539bf28bd4de6aa73b63b
anmedina-arg <andres.medina.arg@gmail.com>
2025-12-02 12:07:23 -0300

    update interface

--- src/components/FilterButtons.tsx
+++ src/components/FilterButtons.tsx
@@ -12,39 +12,6 @@ const FilterButtons: React.FC = () => {
 			<Chips to="#Bebidas" label="Bebidas" icons="🍹" />
 			<Chips to="#Lacteos" label="Lácteos" icons="🐮" />
 			<Chips to="#Almacen" label="Almacén" icons="🛒" />
-			{/* 			
-			<Link href="#Fiambres" className="bg-yellow-400 font-medium text-black py-0.5 px-1 rounded-xl flex items-center justify-center gap-1">
-				<span className="bg-white rounded-full p-0.25">❗</span>
-				<span>Fiambrería</span>
-			</Link>
-			<Link href="#Panaderia" className="bg-orange-300 font-medium text-black py-0.5 px-1 rounded-xl flex items-center justify-center">
-				<span>🍞</span>
-				<span>Panadería</span>
-			</Link>
-			<Link href="#Congelados" className="bg-blue-300 font-medium text-black py-0.5 px-1 rounded-xl flex items-center justify-center">
-				<span>🍗</span>
-				<span>Congelados</span>
-			</Link>
-			<Link href="#Combos" className="bg-orange-500 font-medium text-black py-0.5 px-1 rounded-xl flex items-center justify-center">
-				<span>🍔</span>
-				<span>Combos</span>
-			</Link>
-			<Link href="#Snaks" className="bg-yellow-400 font-medium text-black py-0.5 px-1 rounded-xl flex items-center justify-center">
-				<span>🍟</span>
-				<span>Snaks</span>
-			</Link>
-			<Link href="#Bebidas" className="bg-blue-800 text-white font-medium py-0.5 px-1 rounded-xl flex items-center justify-center">
-				<span>🍹</span>
-				<span>Bebidas</span>
-			</Link>
-			<Link href="#Lacteos" className="bg-white text-black font-medium py-0.5 px-1 rounded-xl flex items-center justify-center">
-				<span>🧀</span>
-				<span>Lácteos</span>
-			</Link>
-			<Link href="#Almacen" className="bg-green-400 font-medium text-black py-0.5 px-1 rounded-xl flex items-center justify-center gap-1">
-				<span className="bg-white rounded-full p-0.25">🛒</span>
-				<span>Almacén</span>
-			</Link> */}
 		</div>
 	)
 };
